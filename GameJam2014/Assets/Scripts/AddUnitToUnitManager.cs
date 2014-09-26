eb4ed6e08fd40ec36a1f716fd96f771eb9dee071
bsell93 <bryantsell@gmail.com>
2014-09-25 22:30:39 -0400

    Created object, added click detection.

---
+++ GameJam2014/Assets/Scripts/AddUnitToUnitManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddUnitToUnitManager : MonoBehaviour {
+
+	private UnitManager uManager;
+
+	void Start()
+	{
+		uManager = GameObject.FindGameObjectWithTag("manager").GetComponent("UnitManager") as UnitManager;
+	}
+
+	void Clicked(Vector3 point)
+	{
+		Debug.Log (point);
+		if (!uManager.unitManager.Contains (gameObject)) {
+			uManager.unitManager.Add (gameObject);
+		}
+	}
+
+	void Update () 
+	{
+	
+	}
+}
---
+++ GameJam2014/Assets/Scripts/UnitManager.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UnitManager : MonoBehaviour 
+{
+	public  List<GameObject> unitManager = new List<GameObject>();
+}
