2888127bb3179ff1e8097122160e7ac35eacfb42
bsell93 <bryantsell@gmail.com>
2014-09-30 00:18:30 -0400

    Ship now moves and slows down to clicked position.

--- GameJam2014/Assets/Scripts/PathFinding.cs
+++ GameJam2014/Assets/Scripts/PathFinding.cs
@@ -7,8 +7,11 @@ public class PathFinding : MonoBehaviour
 	private Vector3 targetPosition;
 
 	public float dampSpeed = 0.5f; // this is the speed at which we rotate toward the point
+	public float slowDrag = 0.977f; // Fine tuned number to get the object to stop at the mouse click
 	public float maxSpeed = 20.0f;
 	public float acceleration = 1.0f;
+	public float goAngle = 5; // Angle at which the object will begin forward motion
+	public float targetPositionTolerance = 2; // Radius when object is "close enough" to mouse click
 
 	public void GoTo(Vector3 pos)
 	{
@@ -17,30 +20,34 @@ public class PathFinding : MonoBehaviour
 		//Set path to true
 		path = true	;
 	}
-	void GotThere()
-	{
-		//Set path to false
-		path = false;
-	}
+
 	void FixedUpdate()
 	{
 		float speed;
 		Vector3 lastPosition;
 
-		//Check speed
-		speed = (transform.position - lastPosition).magnitude;
 		lastPosition = transform.position;
 
 		if(path)
 		{
+			Debug.Log(path);
 			//Look at the position
 			Vector3 relativePos = targetPosition - transform.position;
 			Quaternion rotation = Quaternion.LookRotation (relativePos);
-			gameObject.transform.rotation = Quaternion.Lerp (transform.rotation, rotation, dampSpeed);
-
-			if(speed < maxSpeed)
-			{
-				gameObject.rigidbody.AddForce(acceleration*transform.forward);
+			transform.rotation = Quaternion.Lerp (transform.rotation, rotation, dampSpeed);
+			
+			//Debug.Log(rigidbody.velocity.magnitude < this.maxSpeed);
+			float angle = Quaternion.Angle(transform.rotation, rotation);
+			Debug.Log(angle);
+			if ((transform.position - this.targetPosition).magnitude < 5 && rigidbody.velocity.magnitude > 0) {
+				rigidbody.velocity *= this.slowDrag;
+			} else if(rigidbody.velocity.magnitude < this.maxSpeed && (angle > -this.goAngle && angle < this.goAngle)) {
+				Debug.Log("HERE1");
+				rigidbody.AddForce(transform.forward * this.acceleration);
+			}
+			if((transform.position - this.targetPosition).magnitude < 1) {
+				path = false;
+				Debug.Log("HERE");
 			}
 		}
 	}
