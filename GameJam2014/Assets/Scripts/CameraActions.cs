12b1dfe04be32a2af661fe9b9c2731f22336f2e6
bsell93 <bryantsell@gmail.com>
2014-09-30 16:25:03 -0400

    Made script for camera motion.

---
+++ GameJam2014/Assets/Scripts/CameraActions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraActions : MonoBehaviour
+{
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		if (Input.GetMouseButtonDown(0)) {
+
+		} else if (Input.GetMouseButtonDown(1)) {
+
+		}
+		if (Input.GetAxis("Mouse ScrollWheel")) {
+
+		}
+	}
+}
